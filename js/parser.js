6a5df9dd13d01e1430f583f9894958cd0c10c626
Matthew Koken <maskoken@gmail.com>
2016-10-26 20:17:25 -0700

    js parsing, tracking progress

--- js/parser.js
+++ js/parser.js
@@ -1,22 +1,32 @@
+//break up the classes for checking?
 var coenClasses = [];
 var coreClasses = [];
 
+//or just go and check each individually
+var takenClasses = [];
+
 String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
 
-debugger;
+function sanitize(str) {
+    //sanitize the String
+    return str;
+}
 
 function printClasses() {
     text = "";
     for(i = 0; i < coenClasses.length; i++) {
-        text += coenClasses[i] + "<br>";
+        text += "<div id=\"" + coenClasses[i] + "\">" + coenClasses[i] + "<br></div>";
     }
     for(i = 0; i < coreClasses.length; i++) {
-        text += coreClasses[i] + "<br>";
+        text += "<div id=\"" + coreClasses[i] + "\">" + coreClasses[i] + "<br></div>";
     }
     document.getElementById("allCourses").innerHTML = text;
+    //document.getElementById("allCourses").appendChild(document.createTextNode(text));
+    //appendChild(document.createTextNode(unsafe_str));
 };
 
 function addClasses() {
+    //should sanitize input before handling
     var classNames = document.getElementById("singleInput").value;
     if(checkAddOneOrMany()) {
 
@@ -25,14 +35,33 @@ function addClasses() {
     }
 }
 
+function removeClass() {
+
+}
+
+function arrayIncludes(value, array) {
+    return array.indexOf(value) > -1;
+}
+
 function addClass(className) {
-    var str = className;
+    var str = sanitize(className);
+    
+    //don't add multiple of the same
+    if(!arrayIncludes(takenClasses, className)) {
+        console.log("already added");
+        console.log(takenClasses);
+        return;
+    }
+    
     if(str.includes("coen") || str.includes("COEN")) {
         coenClasses.push(str.toUpperCase());
     } else {
         coreClasses.push(str.toUpperCase());
     }
-    printClasses;
+
+    //should check if a class is actually a class
+    takenClasses.push(str.toUpperCase());
+    printClasses();
 }
 
 //true if add many, false if add one
---
+++ js/tracker.js
@@ -0,0 +1,19 @@
+var fulfilled = [];
+var unfulfilled = [];
+
+function checkRequirements() {
+
+};
+
+function checkCOEN(){
+
+};
+
+function checkCORE(){
+
+};
+
+//print to the fulfilled, unfulfilled boxes
+function printRequirements() {
+
+}
