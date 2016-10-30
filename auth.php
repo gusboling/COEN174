210874323997b3a368cee2569538834c928d6b85
ShadyProphet <geekish2428@gmail.com>
2016-10-29 20:01:29 -0700

    Added load_users function, allows username/password hashes to be retrieved from text file.

--- auth.php
+++ auth.php
@@ -3,6 +3,7 @@
 <head>
 </head>
 <body>
+
 <!--
 PHP block chooses/displays appropriate login procedure based on user type.
 New users will be prompted to create password and input course history. Returning
@@ -11,7 +12,29 @@ entering an invalid student ID will be returned to the login, with an error mess
 displayed.
 -->
 <?php
+  function load_users($source_file){
+    $user_array = array();
 
+    $handle = fopen($source_file, "r") or die("Unable to open file!");
+    if ($handle) {
+        while (($line = fgets($handle)) !== false) {
+            if($line[0] == '#')
+              continue;
+            else {
+              $u_hash = strtok($line, " ");
+              $p_hash = strtok(" ");
+              array_push($user_array, $u_hash, $p_hash);
+            }
+        }
+        fclose($handle);
+    } else {
+        echo "Error Opening User File!";
+    }
+    return $user_array;
+  }
+
+  echo "<script>console.log('PHP Working.')</script>";
+  $previous_users = load_users("data/users.txt");
 ?>
 </body>
 </html>
