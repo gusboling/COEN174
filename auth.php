0bd1cd454d13976e7ee9c1e72394fb0a2b879cac
ShadyProphet <geekish2428@gmail.com>
2016-10-29 18:21:08 -0700

    Prelimary structuring of PHP login procedure

---
+++ auth.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<!--
+PHP block chooses/displays appropriate login procedure based on user type.
+New users will be prompted to create password and input course history. Returning
+users will see a personal dashboard after entering a password. Users
+entering an invalid student ID will be returned to the login, with an error message
+displayed.
+-->
+<?php
+
+?>
+</body>
+</html>
