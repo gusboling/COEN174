9bf75b323cbc3b9204a4ed7f414d971f1e77e107
ShadyProphet <geekish2428@gmail.com>
2016-10-31 00:42:34 -0700

    merge commit

--- writeUser.php
+++ writeUser.php
@@ -69,15 +69,18 @@
     if(isset($_POST['data'])){
       debug_to_console("Data Recieved!");
       $raw_data = $_POST['data'];
+      $class_data = parse($raw_data);
     }
     else{
       debug_to_console("No Data Recieved!");
 
+      /*
       $test_key = "20eabe5d64b0e216796e834f52d61fd0b70332fc";
       $test_data = '["AMTH 106","AMTH 108","CHEM 11","COEN 10","COEN 10L","COEN 11","COEN 11L","COEN 12","COEN 122","COEN 122L","COEN 12L","COEN 146","COEN 146L","COEN 148","COEN 148L","COEN 169","COEN 171","COEN 174","COEN 174L","COEN 177","COEN 177L","COEN 178","COEN 178L","COEN 179","COEN 19","COEN 194","COEN 20","COEN 20L","COEN 21","COEN 21L","COEN 70","COEN 70L","ELEN 153","ELEN 153L","ELEN 50","ELEN 50L","ENGL 181","ENGL 1A","ENGL 2A","ENGR 1","ENGR 111","ENGR 19","ENGR 1L","ETHN 40","HIST 11A","HIST 12A","MATH 11","MATH 12","MATH 13","MATH 14","MATH 53","PHYS 11","PHYS 31","PHYS 32","PHYS 33","POLI 2","RSOC 113","RSOC 9","TESP 82","TRCR 1"]';
       $result_array = parse($test_data);
       $writeResult = write_ctf($test_key ,$result_array);
       echo jstring($result_array);
+      */
     }
   ?>
 </html>
