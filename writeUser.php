ed02ab39152777ef01ab53dc75b19ba25fbe314e
Matthew Koken <maskoken@gmail.com>
2016-10-31 12:19:55 -0700

    return json on read

--- writeUser.php
+++ writeUser.php
@@ -76,7 +76,8 @@
       $user_key = $_POST['user_hash'];
       $user_classes = read_cff($user_key);
       if($user_classes !== false){
-        echo jstring($user_classes);
+        //echo jstring($user_classes);
+        echo json_encode($user_classes);
         //debug_to_console($_POST['user_hash']);
       }
       else{
